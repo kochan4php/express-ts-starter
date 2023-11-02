25e0dc503f35af94ee7afd8fde96c003cd97f75e
kochan4php <aprodeosubarno@gmail.com>
2023-11-02 14:32:00 +0700

    setup unit test and mock the app

---
+++ __mocks__/app.ts
@@ -0,0 +1,6 @@
+import { Application } from 'express';
+import init from '../src/app';
+
+const app: Application = init();
+
+export default app;
---
+++ __tests__/index.spec.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+    it('Should Paus Imut Probolinggo 😁', () => {
+        expect(1 + 1 + 2 + 3 + 5 + 8).toEqual(20);
+    });
+});
